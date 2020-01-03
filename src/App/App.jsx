9faf0992452654d11205f340c91915365c461e52
mbickell <matthew.bicell@nology.io>
2020-01-03 12:39:23 +0000

    Setup finished

---
+++ src/App/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import styles from "./App.module.scss";
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <>
+        <h1>Hello World</h1>
+        <h2>This is a test</h2>
+      </>
+    );
+  }
+}
+
+export default App;
