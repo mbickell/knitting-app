a8a1a4efb1c1dfd29fa06258ffdf4f51c5657b1d
Matthew Bickell <matthew.bickell@Nology.local>
2020-01-19 17:56:25 +0000

    style square based on array values

--- src/components/square/Square.jsx
+++ src/components/square/Square.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import styles from "./Square.module.scss";
 
 const Square = props => {
-  return <span className={styles.square}>{props.type}</span>;
+  return (
+    <span className={styles.square} style={{ backgroundColor: props.type }}>
+      {props.type.length > 1 ? null : props.type}
+    </span>
+  );
 };
 
 export default Square;
--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -4,11 +4,19 @@ import styles from "./App.module.scss";
 import Square from "../../components/square/Square";
 
 const App = () => {
-  let chartSource = [0, 0, 0, 0, 0];
+  let chartSource = ["red", "/", "green", 0, 0];
 
   let chart = chartSource.map(cell => <Square type={cell} />);
 
-  return <div>{chart}</div>;
+  const addValueToChart = () => {
+    // Take input value from input and add it to chartSource array
+  };
+  return (
+    <>
+      <div className={styles.chart}>{chart}</div>
+      <input type="text" name="cell" id="cell" />
+    </>
+  );
 };
 
 export default App;
