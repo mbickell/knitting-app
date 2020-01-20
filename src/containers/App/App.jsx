1bae9e615318a0a88485288f3c7205bf21ba0428
Matthew Bickell <matthew.bickell@Nology.local>
2020-01-20 11:38:56 +0000

    Working prototype

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./App.module.scss";
 
 import Square from "../../components/square/Square";
 
 const App = () => {
-  let chartSource = ["red", "/", "green", 0, 0];
+  const [input, setInput] = useState("");
+  const [chartSource, setChartSource] = useState([]);
 
   let chart = chartSource.map(cell => <Square type={cell} />);
 
-  const addValueToChart = () => {
+  const addValueToChart = event => {
     // Take input value from input and add it to chartSource array
+    event.preventDefault();
+    let modifiedSource = [...chartSource];
+    modifiedSource.push(input);
+    setChartSource(modifiedSource);
   };
   return (
     <>
       <div className={styles.chart}>{chart}</div>
-      <input type="text" name="cell" id="cell" />
+      <form onSubmit={addValueToChart}>
+        <input
+          type="text"
+          value={input}
+          onChange={event => setInput(event.target.value.toLowerCase())}
+          id="cell"
+        />
+        <button type="submit">Add Cell</button>
+      </form>
     </>
   );
 };
