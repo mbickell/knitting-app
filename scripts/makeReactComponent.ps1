df3369edaeb73039cae8de4276b95053a3a56327
Matthew Bickell <matthew.bickell@Nology.local>
2020-01-21 09:24:26 +0000

    Script finished and now writes to file

--- scripts/makeReactComponent.ps1
+++ scripts/makeReactComponent.ps1
@@ -1,5 +1,30 @@
-﻿$type=$args[0]
-$name=$args[1]
+﻿$type = $args[0].ToLower();
+$name = (Get-Culture).TextInfo.ToTitleCase($args[1])
+$jsx = "import React from `"react`";
+import styles from `"./$name.module.scss`";
+
+const $name = () => {
+  return ( 
+    <>
+      <p>$name works</p>
+    </>
+  );
+}
+ 
+export default $name;";
+
+$test = "import React from `"react`";
+import { $name } from `"./$name`";
+
+describe(`"$name tests`", () => {
+  let component;
+
+  beforeEach(() => {
+    component = 
+  })
+});";
 
 md .\src\$type\$name;
-New-Item -ItemType "file" -Path .\src\$type\$name\$name.jsx, .\src\$type\$name\$name.module.scss, .\src\$type\$name\$name.test.js;
+$jsx | Out-File .\src\$type\$name\$name.jsx;
+$test | Out-File .\src\$type\$name\$name.test.js;
+Out-File .\src\$type\$name\$name.module.scss;
---
+++ src/containers/squares/Squares.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Square } from "./Squares";
+
+describe("Squares tests", () => {
+  let component;
+
+  beforeEach(() => {
+    component = 
+  })
+})
