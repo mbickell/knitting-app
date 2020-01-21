2704337ea79f74a2570665dee8366d01679effaf
Matthew Bickell <matthew.bickell@Nology.local>
2020-01-21 08:28:16 +0000

    added powershell script to pakcage.json

---
+++ scripts/makeReactComponent.ps1
@@ -0,0 +1,5 @@
+﻿$type=$args[0]
+$name=$args[1]
+
+md .\src\$type\$name;
+New-Item -ItemType "file" -Path .\src\$type\$name\$name.jsx, .\src\$type\$name\$name.module.scss, .\src\$type\$name\$name.test.js;
